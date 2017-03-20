d744d3ca9ba8f00f67f7cdd4c40c7320b376f6db
Dima Petkov <petkov.dima@gmail.com>
2017-03-19 22:35:35 -0700

    Creates demo project

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
