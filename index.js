4267c2f135e3a4400a82f43dc36bdc60331efe4f
petkov <petkov.dima@gmail.com>
2017-03-19 23:52:47 -0700

    Creates a simple HTTP to MQTT bridge for Heroku deployment

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+var mqtt_host = process.env.MQTT_HOST || '';
+var mqtt_user = process.env.MQTT_USER || '';
+var mqtt_pass = process.env.MQTT_PASS || '';
+var http_port = process.env.HTTP_PORT || 5000;
+
 var express = require('express');
 var app = express();
+var mqtt = require('mqtt');
+var client  = mqtt.connect(mqtt_host, {
+  clientId: 'test',
+  username: mqtt_user,
+  password: mqtt_pass
+});
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', http_port);
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/post/:topic/:content', function(req, res) {
+  res.send('topic:' + req.params['topic'] + ';content:' + req.params['content']);
+  client.publish(req.params['topic'], req.params['content']);
 });
 
 app.listen(app.get('port'), function() {
