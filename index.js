1ad35c7e9317a5fe78bcfd1cfd3969e91508c0b1
petkov <petkov.dima@gmail.com>
2017-03-20 22:54:54 -0700

    Adds authentication.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var auth_key = process.env.AUTH_KEY || '';
 var mqtt_host = process.env.MQTT_HOST || '';
 var mqtt_user = process.env.MQTT_USER || '';
 var mqtt_pass = process.env.MQTT_PASS || '';
@@ -19,6 +20,12 @@ app.set('port', http_port);
 app.use(bodyParser.json());
 
 app.post('/post/', function(req, res) {
+  if (!auth_key || req.body['key'] != auth_key) {
+    console.log('Request is not authorized.');
+    res.send();
+    return;
+  }
+  
   if (req.body['topic']) {
     client.publish(req.body['topic'], req.body['message']);
     res.send('ok');
