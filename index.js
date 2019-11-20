e6af97db3b3b7b91d7bbff77b689b64cdf458db8
vsimonaitis <vilius.simonaitis@codelibre.lt>
2019-11-20 16:11:26 +0200

    Adding grace shutdown to server

--- index.js
+++ index.js
@@ -144,6 +144,30 @@ app.get('/subscribe/', logRequest, authorizeUser, function (req, res) {
     }
 });
 
-app.listen(app.get('port'), function () {
+const server = app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
+});
+
+// The signals we want to handle
+// NOTE: although it is tempting, the SIGKILL signal (9) cannot be intercepted and handled
+var signals = {
+    'SIGHUP': 1,
+    'SIGINT': 2,
+    'SIGTERM': 15
+};
+
+// Do any necessary shutdown logic for our application here
+const shutdown = (signal, value) => {
+    console.log("shutdown!");
+    server.close(() => {
+        console.log(`server stopped by ${signal} with value ${value}`);
+        process.exit(128 + value);
+    });
+};
+// Create a listener for each of the signals that we want to handle
+Object.keys(signals).forEach((signal) => {
+    process.on(signal, () => {
+        console.log(`process received a ${signal} signal`);
+        shutdown(signal, signals[signal]);
+    });
 });
